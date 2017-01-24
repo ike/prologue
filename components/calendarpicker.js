f289985554c24d1b47747a2fb3b0c3ce72c1d865
Isaac Lewis <ike@debian>
2017-01-24 02:47:05 -0500

    redux

--- components/calendarpicker.js
+++ components/calendarpicker.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import moment from 'moment'
 
-export default () => (
-  <select id="calendar">
+const CalendarPicker = ({ calendar, onChange }) => (
+  <select id="calendar" value={ calendar } onchange={ onChange }>
     <option value="new">New Calendar</option>
     <option value="old">Old Calendar</option>
   </select>
 )
+
+CalendarPicker.propTypes = {
+  calendar: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}
+
+export default CalendarPicker
